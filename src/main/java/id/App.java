9d4024443d3915676d8ed08f918a06064049266e
ProgramadorGye <74381886+ProgramadorGye@users.noreply.github.com>
2020-12-03 09:35:26 -0500

    first commit

---
+++ src/main/java/id/App.java
@@ -0,0 +1,25 @@
+package id;
+
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class App {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("JFrame Example");        
+        frame.setSize(600, 300);        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel panel = new JPanel();
+        frame.add(panel);
+        
+        JButton btn1 = new JButton("Hola word"); //crean los elem
+        JLabel lbl1 = new JLabel("Jlabel Example");
+        JCheckBox checkBox = new JCheckBox("opt1");
+        panel.add(btn1); //agregan los elm
+        panel.add(lbl1);
+        panel.add(checkBox);
+        frame.setVisible(true);
+    }
+}
