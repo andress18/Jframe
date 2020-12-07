37247f6f34aa081c6e44312692da30695d12ca64
ProgramadorGye <74381886+ProgramadorGye@users.noreply.github.com>
2020-12-07 16:47:05 -0500

    Calc MVC

--- src/main/java/id/App.java
+++ src/main/java/id/App.java
@@ -15,7 +15,6 @@ import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 
 public class App {
-<<<<<<< HEAD
     // variables y comtenedores
     private JLabel etiquetaSu;
     private JPanel panelIzquierdo, panelDerecho, panelInferior;
@@ -69,23 +68,6 @@ public class App {
         frame.add(panelInferior, BorderLayout.SOUTH);
         // Configuramos el frame
         // frame.pack();
-=======
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("JFrame Example");
-        frame.setSize(600, 300);        
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JPanel panel = new JPanel();
-        frame.add(panel);
-        
-        JButton btn1 = new JButton("Hola word"); //crean los elem
-        JButton btn2 = new JButton("Valerde btn"); //crean los elem
-        JCheckBox checkBox = new JCheckBox("opt1");
-        panel.add(btn1); //agregan los elm
-        panel.add(btn2);
-        panel.add(checkBox);
-        panel.add(new JLabel("Este es un nuevo cambio"));
-        panel.add(new JButton("Button 2"));
->>>>>>> ae49fea4a7b621db254c69bf07a01138a0dbe8b5
         frame.setVisible(true);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
