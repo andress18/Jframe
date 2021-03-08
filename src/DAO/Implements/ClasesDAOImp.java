1a1f1a61f69565634a0f5b329c9a283b7d427b67
Andrés Suárez <andres_s18@outlook.com>
2021-03-08 16:48:50 -0500

    Update ClasesDAOImp.java

--- src/DAO/Implements/ClasesDAOImp.java
+++ src/DAO/Implements/ClasesDAOImp.java
@@ -24,63 +24,67 @@ public class ClasesDAOImp implements ClasesDAO {
 
     @Override
     public void Insertar(Clases value) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet."); // To change body of generated methods, choose
+                                                                       // Tools | Templates.
     }
 
     @Override
     public ArrayList ConsultarTodos(BigDecimal cla) {
         ArrayList lclas = new ArrayList();
-        try{
-                Conexion con = new Conexion();
-                con.Conectar();
-                Statement st = con.getCon().createStatement();
-                Dificultades dificultad = null;
-                String consulta="SELECT * FROM CLASES WHERE ID_TOPICO = "+cla;
-                ResultSet rs = st.executeQuery(consulta);
-                Clases clases=null;
-                
-                while(rs.next()){
-                    clases = new Clases(rs.getBigDecimal("ID"), rs.getString("TEMA"), rs.getString("TEXTOESP"), rs.getString("TEXTOING"), rs.getString("EJEMPLO"), rs.getString("EXPLICACION"));
-                    lclas.add(clases);
-                }
-                rs.close();
-                st.close();
-                con.desconectar();
-                
-            } catch (SQLException ex) {
-                //JLblMensaje.setText(ex.getErrorCode() + "" + ex.getMessage());
+        try {
+            Conexion con = new Conexion();
+            con.Conectar();
+            Statement st = con.getCon().createStatement();
+            Dificultades dificultad = null;
+            String consulta = "SELECT * FROM CLASES WHERE ID_TOPICO = " + cla;
+            ResultSet rs = st.executeQuery(consulta);
+            Clases clases = null;
+
+            while (rs.next()) {
+                clases = new Clases(rs.getBigDecimal("ID"), rs.getString("TEMA"), rs.getString("TEXTOESP"),
+                        rs.getString("TEXTOENG"), rs.getString("EJEMPLO"), rs.getString("EXPLICACION"));
+                lclas.add(clases);
             }
-            finally{
-                return lclas;
+            rs.close();
+            st.close();
+            con.desconectar();
+
+        } catch (SQLException ex) {
+            // JLblMensaje.setText(ex.getErrorCode() + "" + ex.getMessage());
+        } finally {
+            return lclas;
         }
-        
+
     }
 
     @Override
     public Clases Consultar() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet."); // To change body of generated methods, choose
+                                                                       // Tools | Templates.
     }
 
     @Override
     public boolean Eliminar(Clases value) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet."); // To change body of generated methods, choose
+                                                                       // Tools | Templates.
     }
 
     @Override
     public boolean Actualizar(Clases value) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet."); // To change body of generated methods, choose
+                                                                       // Tools | Templates.
     }
 
     @Override
     public List<Clases> ConsultarTodos() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet."); // To change body of generated methods, choose
+                                                                       // Tools | Templates.
     }
 
     @Override
-    public void ConsultarUNO(BigDecimal cla, String titulo, String descripcion, String ejemplo, String textoesp, String textoing) {
-        
+    public void ConsultarUNO(BigDecimal cla, String titulo, String descripcion, String ejemplo, String textoesp,
+            String textoing) {
+
     }
 
-    
-    
 }
